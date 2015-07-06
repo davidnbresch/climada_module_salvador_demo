08a89a5dc1a06a7b44846dfac5b9051514be2ce7
muellele <muellele@gmail.com>
2015-07-06 14:14:49 +0200

    Order first DEM, then event indundation
    in salvador_hazard_FL_prepare.
      - read DEM, plot and save
      - read indundation asci, plot and save
      - indundation hazard consists of
            -- .intensity_masl (water level in meters above sea level)
            -- .intensity_ori (water level in masl - dem)
            -- .intensity (normalized to values between 0 and 4.5m)

--- code/salvador_hazard_FL_prepare.m
+++ code/salvador_hazard_FL_prepare.m
@@ -1,10 +1,11 @@
+
 % prepare inunation hazard for San Salvador Rio Acelhuate (MARN data)
 % salvador inundation hazard
 % but MARN data is based on a 1D visualisation, so data is not useful for
 % flood damage modelling
 
-%% read DEM grid (dem-municipios.txt)
 
+%% read DEM grid (dem-municipios.txt)
 foldername = 'M:\BGCC\CHR\RK\RS\A_Sustainable_Development\Projects\ECA\SanSalvador\consultant_data\hazards\mudslides\system\';
 % asci_file = [foldername 'dem-municipiosll.txt'];
 asci_file = [foldername 'dem-municipios.txt'];
@@ -40,8 +41,47 @@ x_dem = linspace(xllcorner,xllcorner+cellsize*ncols,ncols);
 y_dem = linspace(yllcorner,yllcorner+cellsize*nrows,nrows);
 %         [lon_min, lat_min] = utm2ll_shift(xllcorner, yllcorner);
 %         [lon_max, lat_max] = utm2ll_shift(xllcorner+cellsize*ncols, yllcorner+cellsize*nrows);
+dem_comment = asci_file;
 
-%% read inundation grid (dem-municipiosll.txt)
+
+
+%% create vectors, .lon, .lat, .value
+
+% create meshgrid dem 
+[X, Y ] = meshgrid(x_dem,y_dem);
+dem.X = reshape(X,1,numel(x_dem)*numel(y_dem));
+dem.Y = reshape(Y,1,numel(x_dem)*numel(y_dem));  
+dem.value = reshape(dem_grid,1,numel(x_dem)*numel(y_dem));
+
+% only use dem values that are close to the hazard
+indx_valid = dem.X>473800 & dem.X<482600 & dem.Y>284400 & dem.Y<286500;
+% sum(indx_valid)
+% figure
+% hold on
+% plot(dem.lon(indx_valid),dem.lat(indx_valid),'.')
+dem.X       = dem.X(indx_valid);
+dem.Y       = dem.Y(indx_valid);
+dem.value   = dem.value(indx_valid);
+dem.comment = sprintf('DEM on 10 m resolution, read from %s',dem_comment);
+dem.unit    = 'masl';
+% transformation of UTM to lat lon coordinates
+[dem.lon, dem.lat] = utm2ll_salvador(dem.X, dem.Y);
+
+
+%% plot dem
+figure
+contourf(x_dem,y_dem,dem_grid,[550:10:850],'edgecolor','none')
+colorbar
+% caxis([579 818])
+
+
+%% save dem
+save([climada_global.data_dir filesep 'system' filesep 'dem_san_savlador_10m'],'dem');
+
+
+
+
+%% read inundation grid (TR50A0_test.txt)
 foldername = 'M:\BGCC\CHR\RK\RS\A_Sustainable_Development\Projects\ECA\SanSalvador\consultant_data\hazards\inundation\rio_acelhuate\';
 % asci_file = [foldername 'TR50A0.asc'];
 asci_file = [foldername 'TR50A0_test.asc'];
@@ -78,13 +118,8 @@ x = linspace(xllcorner,xllcorner+cellsize*ncols,ncols);
 y = linspace(yllcorner,yllcorner+cellsize*nrows,nrows);
 
 
-%% plot dem and event inundation
-figure
-contourf(x_dem,y_dem,dem_grid,[550:10:850],'edgecolor','none')
-colorbar
 
-% caxis([579 818])
-% colorbar
+%% plot event inundation
 figure
 % hold on
 contour(x,y,event_grid,[550:10:850])
@@ -93,70 +128,80 @@ colorbar
 figure
 contourf(x,y,event_grid,'edgecolor','none')
 
-%% create vectors, .lon, .lat, .value
-
-% create meshgrid dem 
-[X, Y ] = meshgrid(x_dem,y_dem);
-dem.lon = reshape(X,1,numel(x_dem)*numel(y_dem));
-dem.lat = reshape(Y,1,numel(x_dem)*numel(y_dem));  
-dem.value = reshape(dem_grid,1,numel(x_dem)*numel(y_dem));
-
-% only use dem values that are close to the hazard
-indx_valid = dem.lon>473800 & dem.lon<482600 & dem.lat>284400 & dem.lat<286500;
-% sum(indx_valid)
-% figure
-% hold on
-% plot(dem.lon(indx_valid),dem.lat(indx_valid),'.')
-dem.lon = dem.lon(indx_valid);
-dem.lat = dem.lat(indx_valid);
-dem.value = dem.value(indx_valid);
 
 
-% create meshgrid event_grid 
-[X, Y ] = meshgrid(x,y);
-hazard.lon = reshape(X,1,numel(x)*numel(y));
-hazard.lat = reshape(Y,1,numel(x)*numel(y));  
-hazard.intensity = reshape(event_grid,1,numel(x)*numel(y));
+%% create meshgrid event_grid 
+[X, Y ]  = meshgrid(x,y);
+hazard.X = reshape(X,1,numel(x)*numel(y));
+hazard.Y = reshape(Y,1,numel(x)*numel(y));  
+hazard.intensity      = zeros(size(hazard.X)); %init
+hazard.intensity_ori  = zeros(size(hazard.X)); %init
+hazard.intensity_masl = reshape(event_grid,1,numel(x)*numel(y));
 % use only positive values
-indx_valid = ~isnan(hazard.intensity);
+indx_valid = ~isnan(hazard.intensity_masl);
 % sum(indx_valid)
-hazard.lon = hazard.lon(indx_valid);
-hazard.lat = hazard.lat(indx_valid);
-hazard.intensity = hazard.intensity(indx_valid);
-hazard.dem = zeros(size(hazard.lon));
-hazard.dem = zeros(size(hazard.lon));
+hazard.X   = hazard.X(indx_valid);
+hazard.Y   = hazard.Y(indx_valid);
+hazard.intensity      = hazard.intensity(indx_valid);
+hazard.intensity_ori  = hazard.intensity_ori(indx_valid);
+hazard.intensity_masl = hazard.intensity_masl(indx_valid);
+hazard.dem = zeros(size(hazard.X));
+hazard.dem = zeros(size(hazard.X));
 
+% transformation of UTM to lat lon coordinates
+[hazard.lon, hazard.lat] = utm2ll_salvador(hazard.X, hazard.Y);
 
 
 %% find nearest neighbour and this dem value
-indx = knnsearch([dem.lon' dem.lat'],[hazard.lon' hazard.lat']);
+indx       = knnsearch([dem.X' dem.Y'],[hazard.X' hazard.Y']);
 hazard.dem = dem.value(indx);
 
 % calculate inuundation depht and normalize
-hazard.intensity_ori = hazard.intensity;
-hazard.intensity = hazard.intensity_ori-hazard.dem;
+hazard.intensity_ori  = hazard.intensity_masl-hazard.dem;
 
-% % normalize
-% max_depth = 4.5;
-% hazard.intensity = hazard.intensity+-min(hazard.intensity);
-% hazard.intensity = hazard.intensity/max(hazard.intensity)*max_depth;
+% normalize
+max_depth = 4.5; % just a guess
+hazard.intensity = hazard.intensity_ori -min(hazard.intensity_ori);
+hazard.intensity = hazard.intensity/max(hazard.intensity)*max_depth;
 
 % add hazard fields
-hazard.peril_ID = '';
-hazard.datenum  = now;
-hazard.orig_event_flag = 0;
-hazard.event_ID = 1;
+hazard.frequency = 1/50;
+hazard.peril_ID  = 'FL';
+hazard.datenum   = now;
+hazard.orig_event_flag = 1;
+hazard.event_ID  = 1;
+hazard.unit      = 'm';
+hazard.comment   = 'Modelled by MARN, 1D, 3m resolution, 50 year return period';
+
+
+%% save hazard
+save([climada_global.data_dir filesep 'hazards' filesep 'hazard_Acelhuate_50yr'],'hazard');
+
+
+
+%% plot indundation
+fig = climada_figuresize(0.4,0.8);
+plotclr(hazard.lon, hazard.lat, hazard.intensity,'','',1)
+hold on
+box on
+title({'Flood Rio Acelhuate'; '50 years return period, normalized to 0 and 4.5m '; '1D from MARN, 3m resolution'})
+% plot(hazard.lon(end), hazard.lat(end), 'rx')
+% hazard.intensity(end)
+foldername = sprintf('%sresults%sSanSalvador%sFlood_Rio_Acelhuate_%d_return_period.pdf', filesep,filesep,filesep,1/hazard.frequency(1));
+print(fig,'-dpdf',[climada_global.data_dir foldername])
 
 % climada_hazard_plot_hr(hazard,1,[],[],[],0)
 
-
-%%
-
-% figure
-% plotclr(hazard.lon, hazard.lat, hazard.intensity,'','',1)
-% hold on
+fig = climada_figuresize(0.4,0.8);
+plotclr(hazard.lon, hazard.lat, hazard.intensity_ori,'','',1)
+hold on
+box on
+title({'Flood Rio Acelhuate'; '50 years return period, not normalized '; '1D from MARN, 3m resolution'})
 % plot(hazard.lon(end), hazard.lat(end), 'rx')
 % hazard.intensity(end)
+foldername = sprintf('%sresults%sSanSalvador%sFlood_Rio_Acelhuate_%d_return_period_not_normalized.pdf', filesep,filesep,filesep,1/hazard.frequency(1));
+print(fig,'-dpdf',[climada_global.data_dir foldername])
+
 
 % % just to check for nearest neighbour
 % plot(dem.lon(indx(end)), dem.lat(indx(end)), 'bo','markersize',10)
@@ -166,48 +211,6 @@ hazard.event_ID = 1;
 % 
 % figure
 % plotclr(dem.lon, dem.lat, dem.value)
-
-
-
-
-
-
-
-
-
-
-
-%%
-% switch utm_transformation
-%     case 'barisal'
-%         % only for Barisal: transformation of UTM to lat lon coordinates (including shift)
-%         [lon_min, lat_min] = utm2ll_shift(xllcorner, yllcorner);
-%         [lon_max, lat_max] = utm2ll_shift(xllcorner+cellsize*ncols, yllcorner+cellsize*nrows);
-%         
-%     case 'salvador'
-%         % only for El Salvador: transformation of UTM to lat lon coordinates
-%         [lon_min, lat_min] = utm2ll_salvador(xllcorner, yllcorner);
-%         [lon_max, lat_max] = utm2ll_salvador(xllcorner+cellsize*ncols, yllcorner+cellsize*nrows);  
-%     % original conversion from UTM to lat lon
-%     % [lon_min, lat_min] = btm2ll(xllcorner, yllcorner);
-%     % [lon_max, lat_max] = btm2ll(xllcorner+cellsize*ncols, yllcorner+cellsize*nrows); 
-%     
-%     case '' % without transformation
-%         lon_min = xllcorner;
-%         lat_min = yllcorner;
-%         lon_max = xllcorner+cellsize*ncols;
-%         lat_max = yllcorner+cellsize*nrows;
-% end
-
-
-
-% % create meshgrid
-% [X, Y ] = meshgrid(linspace(lon_min,lon_max,ncols), ...
-%                    linspace(lat_min,lat_max,nrows));
-
-
-
-
 
 
 
