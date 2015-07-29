e6377949c7b05944f99b4886644918a735853a89
muellele <muellele@gmail.com>
2015-07-29 11:17:15 +0200

    Add option for utm in climada_figure_scale_add.m
      - add option for utm (no conversion to lat/lon)

--- code/climada_figure_scale_add.m
+++ code/climada_figure_scale_add.m
@@ -1,4 +1,4 @@
-function climada_figure_scale_add(gca,top_corner,right_corner)
+function climada_figure_scale_add(fig_axes,top_corner,right_corner)
 % add figure scale to a figure 
 % MODULE:
 %   salvador demo
@@ -12,7 +12,7 @@ function climada_figure_scale_add(gca,top_corner,right_corner)
 %   climada_figure_scale_add
 % INPUTS: 
 % OPTIONAL INPUT PARAMETERS:
-%   gca            : get current axis
+%   fig_axes       : axes of figures 
 %   top_corner     : position of scale in the figure, default is 2, scale
 %                    is positioned on second y-grid from the top
 %   right_corner   : position of scale in the figure, default is 1, scale
@@ -21,6 +21,8 @@ function climada_figure_scale_add(gca,top_corner,right_corner)
 %   scale with km information appears on the current axis
 % MODIFICATION HISTORY:
 % Lea Mueller, muellele@gmail.com, 20150724, init
+% Lea Mueller, muellele@gmail.com, 20150729, return if x_tick is empty
+% Lea Mueller, muellele@gmail.com, 20150729, add option for utm (no conversion to lat/lon)
 %-
 
 
@@ -28,20 +30,33 @@ global climada_global
 if ~climada_init_vars,return;end % init/import global variables
 
 % poor man's version to check arguments
-if ~exist('gca'         ,'var'),gca          = []; end
+if ~exist('fig_axes'    ,'var'),fig_axes     = []; end
 if ~exist('top_corner'  ,'var'),top_corner   = []; end
 if ~exist('right_corner','var'),right_corner = []; end
 
-if isempty(gca),gca; end
-if isempty(top_corner)  ,top_corner   = 2; end
-if isempty(right_corner),right_corner = 1; end
+if isempty(fig_axes)    ,fig_axes     = gca; end
+if isempty(top_corner)  ,top_corner   =   2; end
+if isempty(right_corner),right_corner =   1; end
 
+xticks = get(fig_axes, 'xtick');
+yticks = get(fig_axes, 'ytick');
+if isempty(xticks)
+    fprintf('Unable to proceed.\n')
+    return
+end
 
-xticks = get(gca, 'xtick');
-yticks = get(gca, 'ytick');
+% calculate distance between two ticks
+if xticks(1)<=180 
+    % in lat/lon
+    scale_text = sprintf('%2.1f km', climada_geo_distance(xticks(1),yticks(end),xticks(2),yticks(end))/1000);
+else
+    % no conversion, probably in utm in m
+    scale_text = sprintf('%2.1f km', abs(xticks(1)-xticks(2))/1000);
+end
+hold on
 plot(xticks(end-right_corner:end-(right_corner-1)), ones(2,1)*yticks(end-top_corner),'-k','linewidth',3)
-scale_text = sprintf('%2.1f km', climada_geo_distance(xticks(1),yticks(end),xticks(2),yticks(end))/1000);
-text(mean(xticks(end-right_corner:end-(right_corner-1))), yticks(end-top_corner),scale_text,'verticalalignment','bottom','HorizontalAlignment','center','fontsize',14)
+text(mean(xticks(end-right_corner:end-(right_corner-1))), yticks(end-top_corner),scale_text,...
+    'verticalalignment','bottom','HorizontalAlignment','center','fontsize',14)
 
 
 
