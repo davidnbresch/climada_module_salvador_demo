35304f5c9194f6a1cc39f613736463040817ac72
muellele <muellele@gmail.com>
2015-09-15 14:09:39 +0200

    Set disco_mode to 0

--- code/climada_shape_selector.m
+++ code/climada_shape_selector.m
@@ -1,10 +1,9 @@
-function shapes = climada_shape_selector(fig,N,hold_shapes,min_dist_frac,smooth_factor)
+function shapes = climada_shape_selector(fig,N,hold_shapes,min_dist_frac,smooth_factor,shape_file,disco_mode)
 % select shapes
 % NAME:
 %   climada_shape_selector
 % PURPOSE:
-%   Select one or multiple areas in a figure by drawing one or multiple
-%   polygons. Coordinates of polygons are saved in shapes.X and shapes.Y
+%   select shapes in plot
 % CALLING SEQUENCE:
 %   shapes = climada_shape_selector(fig,N,hold_shapes,min_dist_frac)
 % EXAMPLE:
@@ -25,6 +24,8 @@ function shapes = climada_shape_selector(fig,N,hold_shapes,min_dist_frac,smooth_
 %   shapes:     structure array with fields X and Y defining the coordinates
 % MODIFICATION HISTORY:
 % Gilles Stassen, gillesstassen@hotmail.com, 20150729 init
+% Gilles Stassen, 20150827, V2.0 :-)
+% Lea Mueller, muellele@gmail.com, 20150915, set disco_mode to 0
 %-
 
 shapes = struct([]);
@@ -32,11 +33,15 @@ shapes = struct([]);
 global climada_global
 
 if ~climada_init_vars,  return; end % init/import global variables
-if ~exist('fig','var'),  fig = []; end
-if ~exist('N',  'var'),  N   = 1; end
-if ~exist('hold_shapes',  'var'),  hold_shapes   = 1; end
-if ~exist('min_dist_frac',  'var'),  min_dist_frac   = 0.02; end
-if ~exist('smooth_factor',  'var'),  smooth_factor   = 1; end
+if ~exist('fig',            'var'),     fig             = [];           end
+if ~exist('N',              'var'),     N               = 1;            end
+if ~exist('hold_shapes',    'var'),     hold_shapes     = 1;            end
+if ~exist('min_dist_frac',  'var'),     min_dist_frac   = 0.02;         end
+if ~exist('smooth_factor',  'var'),     smooth_factor   = 100;          end
+if ~exist('shape_file',     'var'),     shape_file      = 'NO_SAVE';    end
+if ~exist('disco_mode',     'var'),     disco_mode      = 0;            end
+
+if smooth_factor == 1,  disco_mode = 0; end % disco only when smoothing
 
 % get handles of all existing figures
 figs = findall(0,'Type','Figure');
@@ -63,7 +68,7 @@ hold on
 title_str=get(get(gca,'Title'),'String');
 title_fsz = get(get(gca,'Title'),'FontSize');
 title_ang = get(get(gca,'Title'),'FontAngle');
-title(sprintf('Select %i polygons:',N),'FontSize',14,'FontAngle','Italic')
+title({sprintf('Select %i polygons:',N); 'click in circle to close shape'},'FontSize',14,'FontAngle','Italic')
 
 p = []; % init
 % The try-catch block is to avoid permanent lines on plot when something 
@@ -88,11 +93,20 @@ try
         y_circ  =   min_dist*sin(ang_res);
         circ(n) =   plot(X+x_circ,Y+y_circ,'color','r','linewidth',2);
         
+        % color
+        if disco_mode
+            c_l = getelements(jet(N),n);
+        else
+            c_l = 'r';
+        end
+        
         dist = inf; % init
         
         % loop until user clicks inside circle centered on origin
         while dist > min_dist
             [X(end+1), Y(end+1)] = ginput(1);
+            
+            % to deal with duplicate points which cause issues
             if ismember(X(end),X(1:end-1))
                 X(end) = X(end)*1.00000000001; % small shift
             end
@@ -115,20 +129,51 @@ try
         % parameterisation.
         if smooth_factor >1
             X_ = []; Y_ = []; Sq =[];
+            
+            nS = round(smooth_factor/(length(S)-1));
+            
+            % get the same number of interpolation points for each segment
             for i = 1:length(S)-1
-                Sq(end+1:end+smooth_factor) = linspace(S(i),S(i+1),smooth_factor);
+                Sq(end+1:end+nS) = linspace(S(i),S(i+1),nS);
             end
             X_ = interp1(S,X,Sq,'spline');
             Y_ = interp1(S,Y,Sq,'spline');
             
             X = X_; Y = Y_; clear X_ Y_
+            
             p(end+1) = plot(X, Y,'color','b','linewidth',2);
+            
+            shapes(n).Sq = Sq; % save for disco mode
         end
+                
+        shapes(n).X = X; 
+        shapes(n).Y = Y; 
         
-        shapes(n).X = X; clear X
-        shapes(n).Y = Y; clear Y
+        shapes(n).BoundingBox(:,1) = [nanmin(X) nanmax(X)];
+        shapes(n).BoundingBox(:,2) = [nanmin(Y) nanmax(Y)];
+        
+        shapes(n).NAME = sprintf('shape %i',n);
+        
+        shapes(n).AREA = polyarea(X,Y);
+        
+        clear X
+        clear Y
     end
-    pause(2)
+
+    if smooth_factor > 1
+        legend(p([1 end]), 'selected polygon', sprintf('smoothing parameter = %i',smooth_factor));
+    end
+    
+    pause(1)
+    
+    if ~strcmpi(shape_file,'NO_SAVE')
+        [fP, fN, fE] = fileparts(shape_file);
+        if isempty(fP), fP = climada_global.data_dir;   end
+        if isempty(fE), fE = '.mat';                    end
+        fprintf('saving shapes to %s\n',[fP filesep fN fE])
+        save([fP filesep fN fE],'shapes')
+    end
+
 catch
     % something went wrong, code does not get stuck, but continues to delete lines on plot
     cprintf([1 0 0],'ERROR: aborting\n') 
@@ -146,7 +191,44 @@ if exist('p','var') && ~hold_shapes
     for p_i = 1:length(p)
         delete(p(p_i))
     end
+    legend off
+end
+
+if disco_mode
+    legend off
+    s = []; % init surface handle vector
+    
+    cmap = [jet(round(length(Sq)/2)+1); flipud(jet(round(length(Sq)/2)-1))];
+    colormap(cmap)
+    
+    for n = 1:N
+        X = shapes(n).X;    Y = shapes(n).Y;    Sq = shapes(n).Sq;
+        
+        s(end+1)  = surface([X;X],[Y;Y],ones([2 length(X)]),[Sq;Sq]./max(Sq),'edgecol','interp','linew',5, 'marker','o','markersize',1);
+    end
+    
+    step = max(floor(length(cmap)/10),1);
+    t0 = clock;
+    while etime(clock,t0) < 10
+        cmap = [cmap(end-step:end,:); cmap(1:end-step,:)];
+        colormap(cmap)
+        drawnow
+        title('d(-_-)b DISCO d(-_-)b','FontWeight','Bold','Color',getelements(jet(50),max(floor(rand*50),1)),'interpreter','none')
+        pause(0.1)
+    end
+    
+    % delete surfs
+    for s_i = 1:length(s)
+        delete(s(s_i))
+    end 
+end
+
+% remove Sq field from struct
+if isfield(shapes,'Sq')
+    shapes = rmfield(shapes,'Sq');
 end
 
 % restore title
 title(title_str,'Fontsize',title_fsz,'FontAngle',title_ang)
+
+return
